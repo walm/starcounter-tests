cb7bed1ce4f8aae937b0c1bd7a5e207d6a11b06a
Andreas Wålm <andreas@walm.net>
2015-08-27 21:41:50 +0000

    simple Hello World test with db

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Starcounter;
+
+namespace HelloWorld
+{
+    [Database]
+    public class User
+    {
+        public string Name;
+        public DateTime CreatedAt;
+
+        public User(string name)
+        {
+            Name = name;
+            CreatedAt = DateTime.Now;
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            Handle.GET("/helloworld", () => "Hello World!");
+            Handle.GET("/helloworld/{?}", (string name) => 
+            {
+                User u;
+                u = Db.SQL<User>("SELECT u FROM User as u WHERE u.Name = ?", name).First;
+                if (u == null)
+                {
+                    Db.Transact(() =>
+                    {
+                        u = new User(name);
+                    });
+                }
+                return "Hello " + u.Name + " from " + u.CreatedAt.ToString() + "!";
+            });
+        }
+    }
+}
